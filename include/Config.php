f12c06f4f9fefceb60d87cb39a74690eda6f8e88
dtorresch <109763252+dtorresch@users.noreply.github.com>
2023-11-24 18:16:52 -0500

    Update Config.php

--- include/Config.php
+++ include/Config.php
@@ -3,7 +3,7 @@
 /**
  * Database config variables
  */
-define("DB_HOST", "db4free.net");
+define("DB_HOST", "db4free.net:3306");
 define("DB_USER", "prueba001");
 define("DB_PASSWORD", "T^}7]6Ls%P-Y"nV");
 define("DB_DATABASE", "prueba001");
